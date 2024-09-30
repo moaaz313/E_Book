a9551c5f05eaa1fa4250f64697d69c2dd808915e
moaaz313 <you@example.com>
2024-09-30 04:33:12 +0300

    > using  animation on text in splash screen

--- lib/featuers/splash/widgets/splash_view_body.dart
+++ lib/featuers/splash/widgets/splash_view_body.dart
@@ -1,15 +1,51 @@
 import 'package:e_book/core/helpers/assets.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class SplashViewBody extends StatelessWidget {
+import 'sliding_animation_text.dart';
+
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 10), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [Center(child: Image.asset(AssetsData.logo))],
+      children: [
+        Center(child: Image.asset(AssetsData.logo)),
+        const SizedBox(
+          height: 5,
+        ),
+        SlidingAnimationText(slidingAnimation: slidingAnimation)
+      ],
     );
   }
 }
